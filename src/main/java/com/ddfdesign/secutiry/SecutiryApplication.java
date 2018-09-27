a9975ac2e189e1abeea799cb0fa2159d31998b46
ddfdesign2018 <43002783+ddfdesign2018@users.noreply.github.com>
2018-09-27 11:30:56 +0200

    Add files via upload

---
+++ src/main/java/com/ddfdesign/secutiry/SecutiryApplication.java
@@ -0,0 +1,14 @@
+package com.ddfdesign.secutiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SecutiryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SecutiryApplication.class, args);
+	}
+}
